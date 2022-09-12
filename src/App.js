d6430c72efbd81a826b8451a3aa2469321e7d9e8
Nirmal5678 <43094888+Nirmal5678@users.noreply.github.com>
2022-09-12 20:28:01 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export function MyProject() {
+  return (
+    <div>
+      <h1>My first project</h1>
+      <p> This is my first react project , Yeah! i am learning</p>
+      <ul>
+        <li>Html</li>
+        <li>CSS</li>
+        <li>React</li>
+      </ul>
+    </div>
+  );
+}
